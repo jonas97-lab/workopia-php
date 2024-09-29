7bdcd9d7bdaf6fc22b2d1c6f139f44f78bfd1f88
Jonas <jonas@thuisgekookt.nl>
2024-09-29 08:53:30 +0200

    router refactor

--- public/index.php
+++ public/index.php
@@ -2,5 +2,11 @@
 require '../helpers.php';
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
 require basePath('router.php');
+
+$router = new Router();
+$routes = require basePath('routes.php');
+
+$router->route($uri, $method);
