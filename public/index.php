207017793d63a68dee9cbd78f609b6e2c73df319
Jonas <jonas@thuisgekookt.nl>
2025-01-06 09:36:51 +0100

    authorization

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
-
-require basePath('router.php');
-
+// Instatiate the router
 $router = new Router();
+
+// Get routes
 $routes = require basePath('routes.php');
 
-$router->route($uri, $method);
+// Get current URI and HTTP method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Route the request
+$router->route($uri);
